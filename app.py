7de26f489bcae3a6bfffeefc7b13a90b8059e7c9
debora2veloso <deborafilipa321@gmail.com>
2019-06-19 10:41:17 +0100

    app

--- app.py
+++ app.py
@@ -1,130 +1,130 @@
-from flask import Flask, render_template, request, redirect
-from waitress import serve
-from models import lojaOnline
-
+from flask import Flask, render_template, request
+import psycopg2
 app = Flask(__name__)
 
-# Observação: a loja é uma só para todos os clientes :-(
-loja = lojaOnline()
+def herokudb():
+    Host='ec2-54-247-96-169.eu-west-1.compute.amazonaws.com'
+    Database='d3a8gqg0padli2'
+    User='jdyfyykhyozlly'
+    Password='1d355ded77d6d943c0a5486518c564c97f3397b0e83239ae138ade8aa24ed7c4'
+    return psycopg2.connect(host=Host, database=Database, user=User, password=Password, sslmode='require')
+
+def gravar(v1, v2, v3):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
+    db = ficheiro.cursor()
+    db.execute("CREATE TABLE IF NOT EXISTS usr (nome text,email text, passe text)")
+    db.execute("INSERT INTO usr VALUES (%s, %s, %s)", (v1, v2, v3))
+    ficheiro.commit()
+    ficheiro.close()
+
+
+def existe(v1):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
+    db = ficheiro.cursor()
+    db.execute("SELECT * FROM usr WHERE nome = %s", (v1,))
+    valor = db.fetchone()
+    ficheiro.close()
+    return valor
+
+
+def log(v1, v2):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
+    db = ficheiro.cursor()
+    db.execute("SELECT * FROM usr WHERE nome = %s and passe = %s", (v1, v2,))
+    valor = db.fetchone()
+    ficheiro.close()
+    return valor
+
+
+@app.route('/registo', methods=['GET', 'POST'])
+def route():
+    erro = None
+    if request.method == 'POST':
+        v1 = request.form['utilizador']
+        v2 = request.form['email']
+        v3 = request.form['passe']
+        v4 = request.form['cpasse']
+        if existe(v1):
+            erro = 'O Utilizador já existe.'
+        elif v3 != v4:
+            erro = 'A palavra passe não coincide.'
+        else:
+            gravar(v1, v2, v3)
+    return render_template('registo.html', erro=erro)
+
+
+def alterar(v1, v2):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
+    db = ficheiro.cursor()
+    db.execute("UPDATE usr SET passe = %s WHERE nome = %s", (v2, v1))
+    ficheiro.commit()
+    ficheiro.close()
+
+
+def apaga(v1):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
+    db = ficheiro.cursor()
+    db.execute("DELETE FROM usr WHERE nome = %s", (v1,))
+    ficheiro.commit()
+    ficheiro.close()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', loja=loja)
-
-
-@app.route('/encomenda')
-def encomenda():
-    fatura = loja.fatura
-    loja.reset()
-    return render_template('encomenda.html', loja=loja, fatura=fatura)
-
-
-@app.route('/pr_ini/<produto>')
-def products_ini(produto):
-    loja.pagina = 0
-    target = '/pr/' + produto
-    return redirect(target)
-
-
-@app.route('/pr/<produto>')
-def products(produto):
-    return render_template('produtos.html', loja=loja, folha=loja.livro[produto])
-
-
-@app.route('/add/<produto>/<int:linha>')
-def addcarrinho(produto, linha):
-    loja.addcarrinho(produto, linha)
-    return redirect(request.referrer)
-
-
-@app.route('/mover/<int:valor>')
-def mover(valor):
-    loja.pagina += valor - 1
-    return redirect(request.referrer)
-
-
-@app.route('/pagina/<int:pagina>')
-def pagina(pagina):
-    loja.pagina = pagina - 1
-    return redirect(request.referrer)
-
-
-@app.route('/carrinho_ini')
-def showcarrinho_ini():
-    loja.pagina = 0
-    return redirect('/carrinho')
-
-
-@app.route('/carrinho', methods=['GET', 'POST'])
-def showcarrinho():
-    if request.method == 'POST':
-        loja.usr.nif = request.form['nif']
-        loja.usr.nome = request.form['nome']
-        loja.usr.morada = request.form['morada']
-        loja.usr.update()
-        try:
-            from fatura import Fatura
-            loja.fatura = Fatura().cria_pdf(loja)
-        except:
-            loja.fatura = 'Brevemente.pdf'
-        loja.encomenda()
-        return redirect('/encomenda')
-    return render_template('carrinho.html', loja=loja)
-
-
-@app.route('/encomendar')
-def encomendar():
-    loja.encomendar = True
-    return redirect(request.referrer)
-
-
-@app.route('/sub/<int:linha>')
-def subcarrinho(linha):
-    loja.subcarrinho(linha)
-    return redirect(request.referrer)
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     erro = None
     if request.method == 'POST':
-        usr = request.form['utilizador']
-        passes = loja.usr.passes()
-        # Se o utilizador existe e a passe está correta:
-        if usr in passes and passes[usr] == loja.usr.code(request.form['passe']):
-            loja.usr.set(request.form['utilizador'])
-            return redirect('/')
-        elif usr not in passes:
-            erro = 'O utilizador não existe.'
+        v1 = request.form['utilizador']
+        v2 = request.form['passe']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif not log(v1, v2):
+            erro = 'A palavra passe está errada.'
         else:
-            erro = 'A palavra passe está incorreta.'
-    return render_template('login.html', loja=loja, erro=erro)
+            erro = 'Bem-Vindo.'
+    return render_template('login.html', erro=erro)
 
 
-@app.route('/registo', methods=['GET', 'POST'])
-def registo():
+@app.route('/apagar', methods=['GET', 'POST'])
+def apagar():
     erro = None
     if request.method == 'POST':
-        if request.form['utilizador'] in loja.usr.passes():
-            erro = 'O utilizador já existe.'
-        elif request.form['passe'] != request.form['cpasse']:
+        v1 = request.form['utilizador']
+        v2 = request.form['passe']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif not log(v1, v2):
+            erro = 'A palavra passe está errada.'
+        else:
+            apaga(v1)
+            erro = 'Conta Eliminada com Sucesso.'
+    return render_template('apagar.html', erro=erro)
+
+
+@app.route('/newpasse', methods=['GET', 'POST'])
+def newpasse():
+    erro = None
+    if request.method == 'POST':
+        v1 = request.form['utilizador']
+        v2 = request.form['passe']
+        v3 = request.form['cpasse']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif v2 != v3:
             erro = 'A palavra passe não coincide.'
         else:
-            usr = request.form['utilizador']
-            loja.usr.save(usr, request.form['email'], request.form['passe'])
-            loja.usr.set(usr)
-            return redirect('/')
-    return render_template('registo.html', loja=loja, erro=erro)
+            alterar(v1, v2)
+    return render_template('newpasse.html', erro=erro)
 
-
-@app.route('/logout')
-def logout():
-    loja.reset()
-    return redirect('/')
-
-# Teste de update - Turno 2
 
 if __name__ == '__main__':
     app.run(debug=True)
-    # serve(app)
